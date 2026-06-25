1ebc02c2cd155677e52a5b73917d82385b4e55b6
DCrust369 <DCrust@proton.me>
2026-06-25 15:41:19 +0100

    Add files via upload

---
+++ leyer_notes.zig
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: GPL-3.0-or-later OR Apache-2.0 OR MIT
+// Copyright (c) 2026 DCrust
+// VortexShield-kernel - Focused on Cybersecurity and Pentesting
+
+const std = @import("std"); // i whiting a leyernet language
+
+const pub fn main ()void {
+	.const asm: type = asm_controler;
+	.const Clang: type = C_controler;
+	.const RS: type = rust_controler;
+	.const zig: type = zig_controler;
+};
+
+pub fn keyboards(allocate: u64) void {
+	.var mov: bool = (move_bite_for_hash);
+	.var mut: bool = (manipule_value);
+	.var &mut: bool = (no_mutable);
+    .const term: bool (value_shell);
+    // documentations of the language leyernet
+    // this is a new assembly
+    .const printik: type = see_value;
+    .const fi: type = start,
+    .const main: bool = (start);
+    .const void: bool = No_turning_back,
+};
+
+pub fn keyboards({allocate: i128
+    .const voideless: i8 = never(allocator);
+    .const shell: i8 = (bash(zsh(powershell)));
+    .const godzilla: i8 = (cpu(gpu(controler)));
+    .asm chernel: i8 = kernel_comand,
+});
