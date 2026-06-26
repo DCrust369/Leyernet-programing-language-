7e7ac99059d75214c14e65d971d2fb175dace205
DCrust369 <DCrust@proton.me>
2026-06-26 16:06:26 +0100

    Update register_0x1024.asm

--- low_level/register/register_0x1024.asm
+++ low_level/register/register_0x1024.asm
@@ -1,1 +1,39 @@
+section .data
+    global _start
 
+_start
+    mov rsi, program_data
+    mov rdi, registers
+
+    lea rax, [0x8]
+    register_8 db 8
+    ;
+    lea rax, [0x16]
+    register_16 db 16
+    ;
+    lea rax, [0x32]
+    register_32 db 32
+    ;
+    lea rax, [0x64]
+    register_64 db 64
+    ;
+    lea rax, [0x128]
+    register_128 db 128
+    ;
+    mov rax, 128
+    shl rax, 1
+    lea eax, [0x128]
+    register_128 db 128
+    ;
+    mov rax, 256
+    shl rax, 1
+    lea rax, [0x256]
+    register_256 db 256
+    ;
+    mov rax, 512
+    shl rax, 1
+    mov rax, 1024
+    lea rax, [0x512]
+    register_1024 db 1024
+    ; i exit thiiiiisssssssss. I even got tired doing that aaahh
+    jmp register_256
