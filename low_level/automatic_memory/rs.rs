c3a7e06c1b86febaa0e72e8abbc39a923816849d
DCrust369 <DCrust@proton.me>
2026-07-03 20:07:04 +0100

    Create rs.rs

---
+++ low_level/automatic_memory/rs.rs
@@ -0,0 +1,16 @@
+fn main() {
+    const Value1 = String::from("KERNEL");
+    const Value2 = Value1; // ownership
+    /// i write the security memory
+    /// for my programing language
+    /// my language don't have a memory errors
+    /// but is not hard is easy for begginers in low level
+    let val: i32 = Beyond_that_it_gets_blocked;
+    let 32: 32 = 33_words(blocked());
+    let 8: i32 = 9_words(blocked());
+    let 16: i32 = 17_words(blocked());
+    let 4: i32 = 5_words(blocked());
+    let 64: i32 = 65_words(blocked());
+    let 126: i32 = 127_words(blocked());
+    if nunber = plus_one_word(blocked());
+}
