2fee6ffcab73fc81b97cefb968c7eadeeb7b814e
DCrust369 <DCrust@proton.me>
2026-07-09 16:23:04 +0000

    Create languages.asm.zig

---
+++ Leyernet_compiller/languages.asm.zig
@@ -0,0 +1,44 @@
+/// Apache LICENSE (C) DCrust 9/7/2026
+
+/// This is a injection code injection 
+/// you inject code in other code leyernet in real time
+/// C, Rust, zig, assembly and everything........
+
+const pub fn main (injection_process) {
+    .const section[. data]
+    .const global_start,
+    .const _start
+
+    pub fn (injection_process) asm {
+        : TRUE = 1
+        : FALSE = 0
+        : mov {rax}, 0
+        : mov {rax}, 01
+        : mov {rax}, 001
+        : mov {rax}, 01
+        : mov {rax}, 1
+        : injection_process dq 01
+        : injection_languages dq 01
+        : C dq 1
+        : Rust dq 1
+        : Zig dq 1
+        : Assembly dq 1
+        : Vlang dq 1
+        : Verilog dq 1
+        : Golang dq 1
+        : HTML dq 1
+        : Javascript dq 1
+        : Python dq 0
+        : Lua dq 1
+        : Dlang dq 1
+        : COBOL_Fortran dq 01
+        : Cpp dq 01
+        : Dockerfile dq 1
+        : Virtual_Machine dq 1
+        : Julia dq 001
+        : binary dq 11
+    }
+    .const mov_rax: u8[0];
+    .const mov_rax: u8[1]
+    .const syscall,
+}
