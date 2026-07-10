1926eec68ebf41bf96bc0601d2718550d0535f6d
DCrust369 <DCrust@proton.me>
2026-07-10 23:24:32 +0100

    Create manual_m_Secure_m.asm.zig

---
+++ Keywords/manual_m_Secure_m.asm.zig
@@ -0,0 +1,27 @@
+const pub fn main () void {
+    .const smra: i40 = secure_memory_remote_acess;
+    pub fn (secure_memory_remote_acess) {
+        .var block: i32 = remote(byte);
+	.var block(my_local): i32 = block(->)block(my_local);
+	.const block->ram_memory: i32 = ram_memory->block(my_local);
+	.var *define(5+5): i8 = true;
+	.va *define(): i11 = allocator;
+    }
+    return 0;
+    pub fn (mov_bit_and_byte) asm {
+        : block dq 40
+	: mov {rax}, 45
+	: mov {rax}, 50
+	: mov {rax}, 55
+	: mov {rax}, 60
+	: mov {rax}, 65
+	: mov {rax}, 70
+	: mov {rax}, 75
+	: mov {rax}, 80
+	: block1 dq 80
+	: mov {rax}, 40
+	: mov {rdi}, 80
+	: syscall
+    }
+    return 0;
+}
