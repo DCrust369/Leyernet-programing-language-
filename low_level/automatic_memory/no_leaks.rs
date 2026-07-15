0a02f2e3b2dd4721cf21a5e97e89a241d98e4683
DCrust369 <DCrust@proton.me>
2026-07-15 20:05:26 +0100

    Create no_leaks.rs
    for don't leak codes in low level 
    Rust: don't have a total controler for CPU
    assembly: have a total controler but don't memory safe
    leyernet: everythings

---
+++ low_level/automatic_memory/no_leaks.rs
@@ -0,0 +1,49 @@
+pub fn Dont_leak_code () {
+    // Apache LICENSE (C) DCrust
+    // 15 july of a 2026
+    // SM secure memory
+    use std::arch::asm;
+    fn define_settings () {
+        Pass_at, Define_number, ERROR,
+        Desatible_telemetry, TRUE,
+        Rust_secure_memory, Assembly_control, TRUE,
+        LeyerCode, Low_level_Defined, TRUE,
+    }
+    const *Memory->: type = to("*boxes");
+    let "*boxes"->: type = to("*Number");
+    impl no_overflows {
+        10, TRUE,
+        20, TRUE,
+        30, TRUE,
+        40, TRUE,
+        50, TRUE,
+        60, TRUE,
+        70, TRUE,
+        80, TRUE,
+        90, TRUE,
+        100, TRUE,
+        Infinity, Inline_numbers, Direct_numbers, TRUE
+    }
+    let mut "*Number"->*boxes(): type = define_10(exemple(*pass_to_11()));
+    let mut *pass_to_11(): type = dont_compile(ERROR(ERROR_CODE_NO_COMPILE));
+    let mut x: i64 = 7;
+    unsafe {
+        asm! (
+            "mov {0}",
+            "mov {0}, {1}",
+            "mov {0}, {0}, {1}",
+            "mov {1}",
+            "add {2}",
+            "Quilbits {dq}, {2}",
+            "mov {2}, {0}, {1}",
+        );
+        return 1;
+        fn Bit1-->Bit0 () {
+            #[2BIT_support]
+            let mut bit1_OR_0++ = ++("mov_to");
+            let mut bit1_OR_0-- = --("return_for_local");
+            mod 0BIT_support::1BIT_support::2BIT_support;
+        }
+    }
+    return 1;
+}
