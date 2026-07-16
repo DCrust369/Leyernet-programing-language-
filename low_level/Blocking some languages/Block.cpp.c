8c6281eba0172059a64942fab36e1abc7dfbbf60
DCrust369 <DCrust@proton.me>
2026-07-16 14:06:11 +0100

    Create Block.cpp.c

---
+++ low_level/Blocking some languages/Block.cpp.c
@@ -0,0 +1,18 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+int main(void) {
+    const Cpp = block(C_whith_classes);
+    struct  {
+        int 0 = false;
+        int 1 = true;
+        /*don't compiler for C++ */
+        int* Cpp
+        char* C++
+        //
+        int16_t *filess = classes_c(0);
+        const *filessCpp = calloc(sizeof(int16_t));
+        free(*filessCpp);
+    }
+}
