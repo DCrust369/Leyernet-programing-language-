061510059d803f9ae861f5a5ad1896235ba798e8
DCrust369 <DCrust@proton.me>
2026-07-19 18:38:24 +0100

    Update languages.asm.zig

--- Leyernet_compiller/languages.asm.zig
+++ Leyernet_compiller/languages.asm.zig
@@ -1,44 +1,30 @@
-/// Apache LICENSE (C) DCrust 9/7/2026
-
-/// This is a injection code injection 
-/// you inject code in other code leyernet in real time
-/// C, Rust, zig, assembly and everything........
-
-const pub fn main (injection_process) {
-    .const section[. data]
-    .const global_start,
-    .const _start
-
-    pub fn (injection_process) asm {
-        : TRUE = 1
-        : FALSE = 0
-        : mov {rax}, 0
-        : mov {rax}, 01
-        : mov {rax}, 001
-        : mov {rax}, 01
-        : mov {rax}, 1
-        : injection_process dq 01
-        : injection_languages dq 01
-        : C dq 1
-        : Rust dq 1
-        : Zig dq 1
-        : Assembly dq 1
-        : Vlang dq 1
-        : Verilog dq 1
-        : Golang dq 1
-        : HTML dq 1
-        : Javascript dq 1
-        : Python dq 0
-        : Lua dq 1
-        : Dlang dq 1
-        : COBOL_Fortran dq 01
-        : Cpp dq 01
-        : Dockerfile dq 1
-        : Virtual_Machine dq 1
-        : Julia dq 001
-        : binary dq 11
-    }
-    .const mov_rax: u8[0];
-    .const mov_rax: u8[1]
-    .const syscall,
+// Definindo as capacidades de injeção por linguagem
+struct InjectionConfig {
+    c: bool,
+    rust: bool,
+    zig: bool,
+    assembly: bool,
+    golang: bool,
+    javascript: bool,
+    python: bool,
+    // ... outros campos
 }
+
+const CONFIG: InjectionConfig = InjectionConfig {
+    c: true,
+    rust: true,
+    zig: true,
+    assembly: true,
+    golang: true,
+    javascript: true,
+    python: false, // Conforme sua definição
+};
+
+// Função principal de inicialização
+pub fn main() {
+    // Aqui viria a lógica de attach ao processo alvo
+    // Exemplo conceitual:
+    // let pid = get_target_pid();
+    // inject_code(pid, target_payload);
+}
+//
